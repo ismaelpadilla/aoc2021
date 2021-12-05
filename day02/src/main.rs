a745bf3a2399c78d739e419e626644990d46c169
Ismael Padilla <padillaismael92@gmail.com>
2021-12-05 20:18:00 -0300

    day2 part1

---
+++ day02/src/main.rs
@@ -0,0 +1,53 @@
+use std::fs;
+
+enum Direction {
+    Forward,
+    Up,
+    Down
+}
+
+struct Movement {
+    direction: Direction, 
+    distance: i32
+}
+
+fn main() {
+    let contents = fs::read_to_string("input")
+        .expect("Error while reading file");
+
+    let mut horizontal = 0;
+    let mut vertical = 0;
+
+    for line in contents.lines() {
+        let split: Vec<&str> = line.split(' ').collect();
+
+        let movement = vector_to_movement(split);
+
+        match movement.direction {
+            Direction::Forward => { horizontal += movement.distance },
+            Direction::Up => { vertical -= movement.distance },
+            Direction::Down => { vertical += movement.distance },
+        }
+    }
+
+    println!("Horizontal: {}", horizontal);
+    println!("Vertical: {}", vertical);
+    println!("Total distance: {}", horizontal * vertical);
+}
+
+fn vector_to_movement(vector: Vec<&str>) -> Movement {
+    let direction = match vector[0] {
+        "forward" => Direction::Forward,
+        "up" => Direction::Up,
+        "down" => Direction::Down,
+        _ => panic!("Expected a direction")
+    };
+    Movement {
+        direction,
+        distance: line_to_int(vector[1]),
+    }
+}
+
+fn line_to_int(line: &str) -> i32 {
+    line.parse::<i32>().unwrap()
+}
